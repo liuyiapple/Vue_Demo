43559deb759e89c86f780e1f8c706212b7c7fbc0
‘liuyi’ <1457569132@qq.com>
2022-05-31 18:13:09 +0800

    小球移动

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <BallTransformVue/>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import BallTransformVue from './components/1.BallTransform.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "app",
+    components:{
+        BallTransformVue
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/1.BallTransform.vue
@@ -0,0 +1,55 @@
+<!-- 小球移动的组件 -->
+<template>
+  <div class="containerApp">
+    <span :style="positionSpan"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BallTransformVue",
+  data() {
+    return {
+      leftNum: "",
+      topNum: "",
+      positionSpan: {
+        position: "absolute",
+        top: 0 + "px",
+        left: 0 + "px",
+      },
+      timer: null,
+    };
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      this.leftNum = this.positionSpan.left.slice(
+        0,
+        this.positionSpan.left.length - 2
+      );
+      this.topNum = this.positionSpan.top.slice(
+        0,
+        this.positionSpan.top.length - 2
+      );
+      this.positionSpan.left = Number(this.leftNum) + 0.4 + "px";
+      this.positionSpan.top = Number(this.topNum) + 0.1 + "px";
+      console.log(this.topNum);
+      if (Number(this.topNum) > 600) {
+        clearInterval(this.timer);
+      }
+    }, 2);
+  },
+};
+</script>
+<style scoped lang="less">
+.containerApp {
+  margin: 0;
+  padding: 0;
+  span {
+    display: block;
+    width: 20px;
+    height: 20px;
+    background-color: red;
+    border-radius: 50%;
+  }
+}
+</style>
